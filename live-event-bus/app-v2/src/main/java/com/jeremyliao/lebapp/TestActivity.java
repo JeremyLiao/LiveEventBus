652ed4625eb9db3394d38e45530afdab707ebe37
liaohailiang <liaohailiang@meituan.com>
2019-01-21 21:22:57 +0800

    整体重构，实现了liveeventbus-v1方案

---
+++ live-event-bus/app-v2/src/main/java/com/jeremyliao/lebapp/TestActivity.java
@@ -0,0 +1,73 @@
+package com.jeremyliao.lebapp;
+
+import android.arch.lifecycle.Observer;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jeremyliao.liveeventbus.LiveEventBus;
+
+public class TestActivity extends AppCompatActivity {
+
+    public static final String KEY_TEST_OBSERVE = "key_test_observe";
+    public static final String KEY_TEST_OBSERVE_FOREVER = "key_test_observe_forever";
+    public static final String KEY_TEST_MULTI_THREAD_POST = "key_test_multi_thread_post";
+    public static final String KEY_TEST_MSG_SET_BEFORE_ON_CREATE = "key_test_msg_set_before_on_create";
+
+    public boolean receiveMsgSetBeforeOnCreate = false;
+    public String strResult = null;
+    public int receiveCount = 0;
+
+    private Observer<String> observer = new Observer<String>() {
+        @Override
+        public void onChanged(@Nullable String s) {
+            strResult = s;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LiveEventBus.get()
+                .with(KEY_TEST_OBSERVE, String.class)
+                .observe(this, new Observer<String>() {
+                    @Override
+                    public void onChanged(@Nullable String s) {
+                        strResult = s;
+                    }
+                });
+        LiveEventBus.get()
+                .with(KEY_TEST_OBSERVE_FOREVER, String.class)
+                .observeForever(observer);
+        LiveEventBus.get()
+                .with(KEY_TEST_MULTI_THREAD_POST, String.class)
+                .observe(this, new Observer<String>() {
+                    @Override
+                    public void onChanged(@Nullable String s) {
+                        receiveCount++;
+                    }
+                });
+        testMessageSetBeforeOnCreate();
+    }
+
+    private void testMessageSetBeforeOnCreate() {
+        LiveEventBus.get().with(KEY_TEST_MSG_SET_BEFORE_ON_CREATE, String.class).setValue("msg_set_before");
+        LiveEventBus.get()
+                .with(KEY_TEST_MSG_SET_BEFORE_ON_CREATE, String.class)
+                .observe(this, new Observer<String>() {
+                    @Override
+                    public void onChanged(@Nullable String s) {
+                        receiveMsgSetBeforeOnCreate = true;
+                        strResult = s;
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LiveEventBus.get()
+                .with(KEY_TEST_OBSERVE_FOREVER, String.class)
+                .removeObserver(observer);
+    }
+}
---
+++ live-event-bus/liveeventbus-v1/src/main/java/com/jeremyliao/liveeventbus/LiveEventBus.java
@@ -0,0 +1,178 @@
+package com.jeremyliao.liveeventbus;
+
+import android.arch.lifecycle.ExternalLiveData;
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.LifecycleRegistry;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by liaohailiang on 2019/1/21.
+ */
+
+public final class LiveEventBus {
+
+    private final Map<String, BusLiveData<Object>> bus;
+
+    private LiveEventBus() {
+        bus = new HashMap<>();
+    }
+
+    private static class SingletonHolder {
+        private static final LiveEventBus DEFAULT_BUS = new LiveEventBus();
+    }
+
+    public static LiveEventBus get() {
+        return SingletonHolder.DEFAULT_BUS;
+    }
+
+    public synchronized <T> Observable<T> with(String key, Class<T> type) {
+        if (!bus.containsKey(key)) {
+            bus.put(key, new BusLiveData<>(key));
+        }
+        return (Observable<T>) bus.get(key);
+    }
+
+    public Observable<Object> with(String key) {
+        return with(key, Object.class);
+    }
+
+    public interface Observable<T> {
+
+        void setValue(T value);
+
+        void postValue(T value);
+
+        void postValueDelay(T value, long delay);
+
+        void postValueDelay(T value, long delay, TimeUnit unit);
+
+        void observe(@NonNull LifecycleOwner owner, @NonNull Observer<T> observer);
+
+        void observeSticky(@NonNull LifecycleOwner owner, @NonNull Observer<T> observer);
+
+        void observeForever(@NonNull Observer<T> observer);
+
+        void observeStickyForever(@NonNull Observer<T> observer);
+
+        void removeObserver(@NonNull Observer<T> observer);
+    }
+
+    private static class BusLiveData<T> extends ExternalLiveData<T> implements Observable<T> {
+
+        private class PostValueTask implements Runnable {
+            private Object newValue;
+
+            public PostValueTask(@NonNull Object newValue) {
+                this.newValue = newValue;
+            }
+
+            @Override
+            public void run() {
+                setValue((T) newValue);
+            }
+        }
+
+        @NonNull
+        private final String key;
+        private Map<Observer, Observer> observerMap = new HashMap<>();
+        private Handler mainHandler = new Handler(Looper.getMainLooper());
+
+
+        private BusLiveData(String key) {
+            this.key = key;
+        }
+
+        @Override
+        public void postValue(T value) {
+            mainHandler.post(new PostValueTask(value));
+        }
+
+        @Override
+        public void postValueDelay(T value, long delay) {
+            mainHandler.postDelayed(new PostValueTask(value), delay);
+        }
+
+        @Override
+        public void postValueDelay(T value, long delay, TimeUnit unit) {
+            postValueDelay(value, TimeUnit.MILLISECONDS.convert(delay, unit));
+        }
+
+        @Override
+        public void observe(@NonNull LifecycleOwner owner, @NonNull Observer<T> observer) {
+            super.observe(owner, observer);
+        }
+
+        @Override
+        public void observeSticky(@NonNull LifecycleOwner owner, @NonNull Observer<T> observer) {
+            super.observeSticky(owner, observer);
+        }
+
+        @Override
+        public void observeForever(@NonNull Observer<T> observer) {
+            if (!observerMap.containsKey(observer)) {
+                observerMap.put(observer, new ObserverWrapper(observer));
+            }
+            super.observeForever(observerMap.get(observer));
+        }
+
+        public void observeStickyForever(@NonNull Observer<T> observer) {
+            super.observeForever(observer);
+        }
+
+        @Override
+        public void removeObserver(@NonNull Observer<T> observer) {
+            Observer realObserver = null;
+            if (observerMap.containsKey(observer)) {
+                realObserver = observerMap.remove(observer);
+            } else {
+                realObserver = observer;
+            }
+            super.removeObserver(realObserver);
+            if (!hasObservers()) {
+                LiveEventBus.get().bus.remove(key);
+            }
+        }
+    }
+
+    private static class ObserverWrapper<T> implements Observer<T> {
+
+        private Observer<T> observer;
+
+        public ObserverWrapper(Observer<T> observer) {
+            this.observer = observer;
+        }
+
+        @Override
+        public void onChanged(@Nullable T t) {
+            if (observer != null) {
+                if (isCallOnObserve()) {
+                    return;
+                }
+                observer.onChanged(t);
+            }
+        }
+
+        private boolean isCallOnObserve() {
+            StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+            if (stackTrace != null && stackTrace.length > 0) {
+                for (StackTraceElement element : stackTrace) {
+                    if ("android.arch.lifecycle.LiveData".equals(element.getClassName()) &&
+                            "observeForever".equals(element.getMethodName())) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
