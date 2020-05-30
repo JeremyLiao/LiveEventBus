51f97c65c7272cb36bc57c79de0cc5c9c770b5a7
Jeremy Liao <190545925@qq.com>
2020-05-30 17:08:59 +0800

    消息基类优化

--- branchs/live-event-bus-x/liveeventbus-x/src/main/java/com/jeremyliao/liveeventbus/core/LiveEvent.java
+++ branchs/live-event-bus-x/liveeventbus-x/src/main/java/com/jeremyliao/liveeventbus/core/LiveEvent.java
@@ -1,7 +1,9 @@
 package com.jeremyliao.liveeventbus.core;
 
+import java.io.Serializable;
+
 /**
  * Created by liaohailiang on 2020-05-30.
  */
-public interface LiveEvent {
+public interface LiveEvent extends Serializable {
 }
