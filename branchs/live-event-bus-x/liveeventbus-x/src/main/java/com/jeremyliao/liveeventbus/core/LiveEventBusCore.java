a57a5edec6f80693f59fe783b1c153c4c8590603
ppjun <953386166@qq.com>
2023-12-25 11:11:02 +0800

    适配Android 14

--- branchs/live-event-bus-x/liveeventbus-x/src/main/java/com/jeremyliao/liveeventbus/core/LiveEventBusCore.java
+++ branchs/live-event-bus-x/liveeventbus-x/src/main/java/com/jeremyliao/liveeventbus/core/LiveEventBusCore.java
@@ -1,6 +1,7 @@
 package com.jeremyliao.liveeventbus.core;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
@@ -95,6 +96,7 @@ public final class LiveEventBusCore {
      * first of all, call config to get the Config instance
      * then, call the method of Config to config LiveEventBus
      * call this method in Application.onCreate
+     *
      * @return Config
      */
     public Config config() {
@@ -124,7 +126,11 @@ public final class LiveEventBusCore {
         if (application != null) {
             IntentFilter intentFilter = new IntentFilter();
             intentFilter.addAction(IpcConst.ACTION);
-            application.registerReceiver(receiver, intentFilter);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                application.registerReceiver(receiver, intentFilter, Context.RECEIVER_EXPORTED);
+            } else {
+                application.registerReceiver(receiver, intentFilter);
+            }
             isRegisterReceiver = true;
         }
     }
